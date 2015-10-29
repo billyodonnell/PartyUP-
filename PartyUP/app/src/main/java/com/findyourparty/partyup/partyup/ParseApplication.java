3ad0e2a323236d28bc254be16868e58621c31f71
billyodonnell <billylolers@gmail.com>
2015-10-29 13:00:43 -0700

    PhoneGap Removal, Android addition

---
+++ PartyUP/app/src/main/java/com/findyourparty/partyup/partyup/ParseApplication.java
@@ -0,0 +1,33 @@
+package com.findyourparty.partyup.partyup;
+
+/**
+ * Created by billy_000 on 10/29/2015.
+ */
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+import com.parse.ParseUser;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Add your initialization code here
+        Parse.initialize(this, "uMwiZTIJCVqhnSnRYWBv0u3mPTA4C1lJuywrEAko", "42sQjyzV6lqF5kzmb1oNoDgaI4PArhDoMbg2WywU");
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this
+        // line.
+        defaultACL.setPublicReadAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+
+}
